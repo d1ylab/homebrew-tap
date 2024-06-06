9d0d9804f7e73af0799a2c0f7efaf2481e2564bd
d1y <chenhonzhou@gmail.com>
2024-06-07 01:12:48 +0800

    updating formula for scls

--- scls.rb
+++ scls.rb
@@ -1,9 +1,9 @@
 class Scls < Formula
   desc "Language server to enable word completion and snippets for Helix editor"
   homepage "http://github.com/d1y/scls"
-  url "https://github.com/d1y/scls/releases/download/v1.0.4/scls-v1.0.4-x86_64-macos.tar.xz"
-  version "1.0.4"
-  sha256 "f578193a02edee95747b27968837a19c29a57495cbd6c125306d9567786428f7"
+  url "https://github.com/d1y/scls/releases/download/v1.0.5/scls-v1.0.5-x86_64-macos.tar.xz"
+  version "1.0.5"
+  sha256 "ec62fe91f8baac030802b657ebe01173366c6bdf8a03f72c4cc86b7e65f54870"
 
   def install
     bin.install "simple-completion-language-server"
